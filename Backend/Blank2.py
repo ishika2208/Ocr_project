9c91fea27e3c57ea24a77337a2fc7c154cabb41d
ishika2208 <ishiigupta.2208@gmail.com>
2023-07-26 14:07:05 +0530

    new changes

--- Backend/Blank2.py
+++ Backend/Blank2.py
@@ -4,49 +4,50 @@ import keras_ocr
 import cv2
 import math
 import numpy as np
+import sys
 
 def midpoint(x1, y1, x2, y2):
-    x_mid = int((x1 + x2)/2)
-    y_mid = int((y1 + y2)/2)
+    x_mid = int((x1 + x2) / 2)
+    y_mid = int((y1 + y2) / 2)
     return (x_mid, y_mid)
 
-
-def inpaint_text(img_path, pipeline):
+def inpaint_text(img_path, pipeline, output_path):
     # read image
     img = keras_ocr.tools.read(img_path)
-    # generate (word, box) tuples 
+    # generate (word, box) tuples
     prediction_groups = pipeline.recognize([img])
     mask = np.zeros(img.shape[:2], dtype="uint8")
     for box in prediction_groups[0]:
         x0, y0 = box[1][0]
-        x1, y1 = box[1][1] 
+        x1, y1 = box[1][1]
         x2, y2 = box[1][2]
-        x3, y3 = box[1][3] 
-        
+        x3, y3 = box[1][3]
+
         x_mid0, y_mid0 = midpoint(x1, y1, x2, y2)
-        x_mid1, y_mi1 = midpoint(x0, y0, x3, y3)
-        
-        thickness = int(math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 ))
-        
-        cv2.line(mask, (x_mid0, y_mid0), (x_mid1, y_mi1), 255,    
-        thickness)
+        x_mid1, y_mid1 = midpoint(x0, y0, x3, y3)
+
+        thickness = int(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
+
+        cv2.line(mask, (x_mid0, y_mid0), (x_mid1, y_mid1), 255, thickness)
         img = cv2.inpaint(img, mask, 7, cv2.INPAINT_NS)
-                 
-    return(img)
 
-pipeline = keras_ocr.pipeline.Pipeline()
+    # Color correction step
+    img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(output_path, img_bgr)
 
-image_path = "C:/Users/gparu/OneDrive/Desktop/testttt.jpg"
-output_image = inpaint_text(image_path, pipeline)
-        
-orignal_image = cv2.imread(image_path)
-cv2.imshow('Original image',orignal_image)
+    print("Text inpainting completed. Output image saved at:", output_path)
 
-img_rgb = cv2.cvtColor(output_image, cv2.COLOR_BGR2RGB)
-cv2.imshow('text_free_image', img_rgb)
+if __name__ == "__main__":
+    pipeline = keras_ocr.pipeline.Pipeline()
 
-#cv2.imshow('Output Image', output_image)
+    if len(sys.argv) != 3:
+        print("Usage: python script.py input_image_path output_image_path")
+        sys.exit(1)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-                 
+    input_image_path = sys.argv[1]
+    output_image_path = sys.argv[2]
+
+    inpaint_text(input_image_path, pipeline, output_image_path)
+
+
+#python (filename.py) (input-image-path) (output-image-path)
